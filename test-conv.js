a4d1739a5df8ca05bef7765fb154e6e7827b5c01
volkanakbulut73-cmyk <volkanakbulut73@gmail.com>
2026-09-07 05:09:38 +0300

    chore: remove test files

--- test-conv.js
+++
@@ -1,8 +0,0 @@
-﻿const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('https://qybzidylewzsnmlofjul.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5YnppZHlsZXd6c25tbG9manVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyNTk3MTMsImV4cCI6MjA5NTgzNTcxM30.WNnzSFMEueVJg_TLaWXdpkadKkw-fJk0vSyNBdHbPrU');
-
-async function test() {
-  const { data, error } = await supabase.from('conversations').select('*').limit(1);
-  console.log(error || Object.keys(data[0] || {}));
-}
-test();
